606532dd1e02365fb27d53e8252f7a4d1e35ec8f
Mihaela Grubii <mihaela.grubii@mail.ru>
2020-05-17 20:07:23 +0300

    Adding .docx functionality

--- main/filegenerator.py
+++ main/filegenerator.py
@@ -1,6 +1,9 @@
-
+import docx
+import urllib.request
+from bs4 import BeautifulSoup
 import os.path
 from os import path
+from docx import Document
 
 counter = 0
 
@@ -13,14 +16,105 @@ def addToFile(name, content, nbr):
             name = name.replace(char, '')
 
     name = name + '.txt'
-    counter += 1
+    global counter
 
-    if path.exists(name):
-        f = open(name, 'a+')
-    else:
-        f = open(name, 'w+')
-        f.write('<<<This are the top ' + str(nbr) +
-                ' results for your search!>>>\n')
-        f.write('Topic: ' + topic.upper() + '\n')
-    f.write('nbr.' + str(counter) + ' result: ' + content + '\n')
-    f.close()
+    for el in content:
+        counter += 1
+        if path.exists(name):
+            f = open(name, 'a+')
+        else:
+            f = open(name, 'w+')
+            f.write('<<<This are the top ' + str(nbr) +
+                    ' results for your search!>>>\n')
+            f.write('Topic: ' + topic.upper() + '\n')
+        f.write('nbr.' + str(counter) + ' result: ' + el + '\n')
+        f.close()
+
+
+def add_hyperlink(paragraph, url, text, color, underline):
+    """
+    A function that places a hyperlink within a paragraph object.
+
+    :param paragraph: The paragraph we are adding the hyperlink to.
+    :param url: A string containing the required url
+    :param text: The text displayed for the url
+    :return: The hyperlink object
+    """
+
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(
+        url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+
+    # Create a new w:rPr element
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Add color if it is given
+    if not color is None:
+        c = docx.oxml.shared.OxmlElement('w:color')
+        c.set(docx.oxml.shared.qn('w:val'), color)
+        rPr.append(c)
+
+    # Remove underlining if it is requested
+    if not underline:
+        u = docx.oxml.shared.OxmlElement('w:u')
+        u.set(docx.oxml.shared.qn('w:val'), 'none')
+        rPr.append(u)
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    paragraph._p.append(hyperlink)
+
+    return hyperlink
+
+
+def addToDocx(name, content, nbr):
+    topic = name
+    name = (name.replace(' ', '')[:25])
+    for char in name:
+        if char in " ?.!/;:":
+            name = name.replace(char, '')
+
+    name = name + '.docx'
+
+    document = Document()
+    row = nbr + 1  # Number of rows you want
+    col = 2  # Number of collumns you want
+    table = document.add_table(rows=row, cols=col)
+    # set your style, look at the help documentation for more help
+    table.style = 'LightShading-Accent1'
+    for y in range(row):
+        for x in range(col):
+            cell = table.cell(y, x)
+            if y == 0:
+                if x == 0:
+                    continue
+                else:
+                    cell.text = '<<This are the top ' + \
+                        str(nbr) + ' results for your search!>>>\n' + \
+                        'Topic: ' + topic.upper()
+            else:
+                if x == 0:
+                    cell.text = 'nbr.' + str(y)
+                else:
+                    try:
+                        soup = BeautifulSoup(
+                            urllib.request.urlopen(content[y - 1]), 'lxml')
+                        subj = str(soup.title.string)
+                    except:
+                        subj = "This is a main page without title!"
+                    p = cell.add_paragraph()
+                    hyperlink = add_hyperlink(
+                        p, content[y - 1], subj, '8822ff', True)
+                    cell.content = hyperlink
+    document.save(name)
--- main/inputreader.py
+++ main/inputreader.py
@@ -2,6 +2,7 @@
 #
 import sys
 from filegenerator import addToFile
+from filegenerator import addToDocx
 from googlesearch import search
 
 print('Argument List:', str(sys.argv))
@@ -10,6 +11,7 @@ searched_topic = list(sys.argv)
 del searched_topic[0]
 nbr_searches = int(searched_topic[-1])
 del searched_topic[-1]
+topics = []
 
 if len(searched_topic) == 0:
     print("Try again, and try to give some arguments to fire up the search.")
@@ -18,4 +20,5 @@ else:
     print(query)
     for i in search(query, tld='com', lang='en', tbs='0', safe='off', num=nbr_searches, start=0, stop=nbr_searches, domains=None, pause=2.0, tpe='', country='', extra_params=None, user_agent=None):
         print(i)
-        addToFile(query, i, nbr_searches)
+        topics.append(i)
+    addToDocx(query, topics, nbr_searches)
